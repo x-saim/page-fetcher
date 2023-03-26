29b4a46b3d4cccaa74c9300ba20f2c78b662effc
Saim Amir <saimxamir@gmail.com>
2023-03-26 00:39:28 -0400

    add request and writeFile functionality

--- fetcher.js
+++ fetcher.js
@@ -12,14 +12,25 @@ const request = require('request');
 const fs = require('fs');
 
 let args = process.argv;
-args = args.slice(2); //reassigning array to args and ignoring the  node executable and file path.
+args = args.slice(2); //reassigning array to args and ignoring the node executable and file path.
 
 //args[0] = URL index
 //args[1] = filepath
-const fetcher = (args[0], args[1]) => {
 
-  request(args[0], (error, response, body)) => {
-    //
+const fetcher = (ULR, filepath) => {
+
+  request(args[0], (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  
+  fs.writeFile(args[1], body, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+
   }
 
-};
+)};
+
+fetcher(args[0], args[1]);
